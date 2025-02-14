5a3905aaa20f9cbe11885d2341807896662451eb
719226093 <67672444+719226093@users.noreply.github.com>
2025-02-14 08:26:25 +0800

    script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const buttonContainer = document.getElementById('buttonContainer');
 
 // 按钮配置数据 (您可以根据需要修改)
 const buttonsData = [
-    { text: "Offer 1 1", url: "https://mostourigoatik.com/4/8904850" }, // 替换为您的 Monetag 链接
-    { text: "Offer 2 2", url: "https://mostourigoatik.com/4/8905048" }, // 替换为您的 Monetag 链接
+    { text: "Offer 1", url: "https://mostourigoatik.com/4/8904850" }, // 替换为您的 Monetag 链接
+    { text: "Offer 2", url: "https://mostourigoatik.com/4/8905048" }, // 替换为您的 Monetag 链接
     { text: "Offer 3", url: "https://mostourigoatik.com/afu.php?zoneid=8904863&var=8904863&rid=XXz4jyvWNyEJSqRKY8d18w%3D%3D&rhd=false&ab2r=0&sf=1" }  // 替换为您的 Monetag 链接
     // ... 更多按钮
 ];
