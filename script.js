a9eb4954b82920191891e488fe856d3e9da5cb30
719226093 <67672444+719226093@users.noreply.github.com>
2025-02-21 11:28:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ const buttonContainer = document.getElementById('buttonContainer');
 
 // 按钮配置数据 (您可以根据需要修改)
 const buttonsData = [
-    { text: "Offer 1", url: "https://theeghumoaps.com/4/8940769" }, // 替换为您的 Monetag 链接
-    { text: "Offer 2", url: "https://theeghumoaps.com/4/8940779" }, // 替换为您的 Monetag 链接
+    { text: "Offer 1", url: "https://nanoushaks.net/4/8985364" }, // 替换为您的 Monetag 链接
+    { text: "Offer 2", url: "https://nanoushaks.net/4/8985363" }, // 替换为您的 Monetag 链接
+    { text: "Offer 3", url: "https://nanoushaks.net/4/8985362" }, // 替换为您的 Monetag 链接
+    { text: "Offer 4", url: "https://nanoushaks.net/4/8985314" }, // 替换为您的 Monetag 链接
+    { text: "Offer 5", url: "https://nanoushaks.net/4/8985312" }, // 替换为您的 Monetag 链接
+    { text: "Offer 6", url: "https://nanoushaks.net/4/8985313" }, // 替换为您的 Monetag 链接
+    { text: "Offer 7", url: "https://nanoushaks.net/4/8985310" }, // 替换为您的 Monetag 链接
+    { text: "Offer 8", url: "https://nanoushaks.net/4/8985307" }, // 替换为您的 Monetag 链接
+    { text: "Offer 9", url: "https://nanoushaks.net/4/8985375" }, // 替换为您的 Monetag 链接
+    { text: "Offer 10", url: "https://nanoushaks.net/4/8985309" }, // 替换为您的 Monetag 链接
     // ... 更多按钮
 ];
 
